57fe46ea9b67f312e8a51c7dbbf29158049623e4
Will Darkins <willdarkins@gmail.com>
2021-10-21 14:01:24 -0700

    routes set and sql commands added

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,21 @@
-CREATE TABLE candidates (
+-- This will drop/delete the tables every time you run the schema.sql file, ensuring that you start with a clean slate.
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    name VARCHAR(50) NOT NULL,
+    description TEXT
 );
+
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+  --CONSTRAINT allows us to flag the party_id field as an official foreign key and tells SQL which table and field it references.
+  --In this case, it references the id field in the parties table.
+  --This ensures that no id can be inserted into the candidates table if it doesn't also exist in the parties table.
--- server.js
+++ server.js
@@ -70,7 +70,12 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -87,7 +92,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
